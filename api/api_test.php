0025c4d405e7d2e494226a29b401ebbd92dbdb6c
AdityaManggala334 <adityamanggala.p334@student.uns.ac.id>
2026-04-28 01:20:41 +0700

    Delete api/api_test.php

--- api/api_test.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "<h2>Debug Query String</h2>";
-echo "REQUEST_URI: " . $_SERVER['REQUEST_URI'] . "<br>";
-echo "QUERY_STRING: " . $_SERVER['QUERY_STRING'] . "<br>";
-echo "<pre>";
-print_r($_GET);
-echo "</pre>";
-?>
