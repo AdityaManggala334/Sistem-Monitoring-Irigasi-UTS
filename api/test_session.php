121ac6fa0c79b1f186ffc0373f115aa8f77193aa
AdityaManggala334 <adityamanggala.p334@student.uns.ac.id>
2026-04-27 13:40:42 +0700

    Update test_session.php

--- api/test_session.php
+++ api/test_session.php
@@ -1,11 +1,3 @@
 <?php
-// File: api/test_info.php
-// Tujuan: Memeriksa apakah runtime PHP Vercel berfungsi.
-
-echo "=== PHP Runtime Test ===";
-echo "<br>Jika Anda melihat teks ini, PHP berjalan dengan baik.";
-echo "<br>Versi PHP: " . phpversion();
-echo "<br>Parameter GET yang diterima: <pre>";
-print_r($_GET);
-echo "</pre>";
+echo "PHP is working!";
 ?>
