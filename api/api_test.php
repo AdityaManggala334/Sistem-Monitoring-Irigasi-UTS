68e76aaed3b540b4f4ba18773f2ab18057926439
AdityaManggala334 <adityamanggala.p334@student.uns.ac.id>
2026-04-27 12:19:20 +0700

    Update api_test.php

--- api/api_test.php
+++ api/api_test.php
@@ -1,9 +1,8 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-echo '<h1>Test Parameter</h1>';
-echo '<pre>';
+echo "<h2>Debug Query String</h2>";
+echo "REQUEST_URI: " . $_SERVER['REQUEST_URI'] . "<br>";
+echo "QUERY_STRING: " . $_SERVER['QUERY_STRING'] . "<br>";
+echo "<pre>";
 print_r($_GET);
-echo '</pre>';
+echo "</pre>";
 ?>
